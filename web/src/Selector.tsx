4f4679432585f4158af5494ffac0f0904a01bcc0
michioxd <michio.haiyaku@gmail.com>
2024-07-12 16:36:10 +0700

    remove icon + add package/publish command

--- web/src/Selector.tsx
+++ web/src/Selector.tsx
@@ -67,7 +67,7 @@ const IconItem = memo(({ icon, onClick }: { icon: IconType, onClick: () => void 
             },
             {
                 label: 'Copy import snippet', onClick: () => {
-                    copy(`import ${icon.importName}Icon from '@mui/icons-material/${icon.importName}Icon';`);
+                    copy(`import ${icon.importName}Icon from '@mui/icons-material/${icon.importName}';`);
                 }
             }
         ]);
